c69d14b56f2c4cf9851422954b96748cda1bfdf5
Danagul Akpaeva <aidanagul10@gmail.com>
2020-10-17 22:32:51 +0500

    2 laba

--- graph.py
+++ graph.py
@@ -2,10 +2,10 @@ import numpy
 
 
 class Graph:
-    def __init__(self, v, e):
+    def __init__(self, v, e, graph_list):
         self.number_of_v = v
         self.number_of_e = e
-        self.graph_list = self.get_list()
+        self.graph_list = graph_list  # self.get_list()
         self.matrix_incidence = self.get_matrix_incidence()
         self.matrix_adjacency = self.get_matrix_adjacency()
         self.semi_exodus = self.get_semi_exodus()
@@ -29,9 +29,13 @@ class Graph:
         gl = self.graph_list
         matrix = numpy.array([[0] * self.number_of_e] * self.number_of_v)
         for i in range(self.number_of_e):
-            if gl[i][0] != gl[i][1]:
-                matrix[gl[i][0]][i] = -1
-                matrix[gl[i][1]][i] = 1
+            matrix[gl[i][0]][i] = -1
+            matrix[gl[i][1]][i] = 1
+            # if gl[i][0] != gl[i][1]:
+            #     matrix[gl[i][0]][i] = -1
+            #     matrix[gl[i][1]][i] = 1
+            # else:
+            #     matrix[gl[i][0]] = 2
         return matrix
 
     def print_matrix(self, mas):
@@ -54,18 +58,28 @@ class Graph:
     # Получение полустепени исхода
     def get_semi_exodus(self):
         semi_exodus = []
-        for i in self.matrix_incidence:
-            semi_exodus.append(numpy.count_nonzero(-1 == i))
+        for i in self.matrix_adjacency:
+            semi_exodus.append(numpy.count_nonzero(1 == i))
         return semi_exodus
 
     # Получение полустепени захода
     def get_half_degree_of_approach(self):
         half_degree_of_approach = []
-        for i in self.matrix_incidence:
+        mt_adj_tr = self.matrix_adjacency.transpose()
+        for i in mt_adj_tr:
             half_degree_of_approach.append(numpy.count_nonzero(1 == i))
         return half_degree_of_approach
 
     # определяет число маршрутов заданной длины
     def number_of_routes(self, length, v1, v2):
         matrix = numpy.linalg.matrix_power(self.matrix_adjacency, length)
-        return matrix[v1][v2]
+        # return matrix[v1][v2]
+        return matrix
+
+    def degree_vertex(self, v):
+        deg_is = 0
+        deg_z = 0
+        for i in range(len(self.matrix_adjacency)):
+            deg_is += self.matrix_adjacency[v][i]
+            deg_z += self.matrix_adjacency[i][v]
+        print('Степень исхода вершины{} = {}\nСтепень захода вершины{} = {}\n'.format(v, deg_is, v, deg_z))
--- main.py
+++ main.py
@@ -1,19 +1,78 @@
 from graph import Graph
-import numpy as np
+import numpy
 import networkx
 import matplotlib.pyplot as plt
 
+#
+# def laba1():
+#     graph_list = [[0, 1],
+#                   [0, 2],
+#                   [1, 1],
+#                   [1, 4],
+#                   [3, 2],
+#                   [4, 3],
+#                   [5, 4],
+#                   [4, 0],
+#                   [5, 0],
+#                   [5, 5]]
+#     g = Graph(6, 10, graph_list)
+#     g()
+#     g.print_matrix(g.number_of_routes(4, 12, 2))
+#
+#
+# def laba2():
+#     gl1 = [[0, 3],
+#            [3, 0],
+#            [3, 1],
+#            [2, 3],
+#            [3, 2],
+#            [2, 1],
+#            [1, 2],
+#            [0, 0],
+#            [1, 1]]
+#     gl2 = [[0, 2], [2, 1], [1, 0]]
+#     g1 = Graph(4, 9, gl1)
+#     g2 = Graph(3, 3, gl2)
+#     g1()
+#     g2()
+
+
+def extend_matrix(matrix, v):
+    ext_matrix = numpy.array([[0] * v] * v)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            ext_matrix[i][j] = matrix[i][j]
+    return ext_matrix
+
+
+def obedinen(g1: Graph, g2: Graph):
+    if g1.number_of_v > g2.number_of_v:
+        g2.matrix_adjacency = extend_matrix(g2.matrix_adjacency, g1.number_of_v)
+
+    if g1.number_of_v < g2.number_of_v:
+        g1.matrix_adjacency = extend_matrix(g1.matrix_adjacency, g2.number_of_v)
+# какая-то хрень
+    # martix = numpy.array([[0] * g1.number_of_v] * g1.number_of_v)
+    #
+    # for i in range(len(g1.matrix_adjacency)):
+    #     for j in range(len(g1.matrix_adjacency)):
+    #         ext_matrix[i][j] = matrix[i][j]
+
+
+
 
 if __name__ == '__main__':
-    g = Graph(3, 4)
-    g()
-    draw_graw = networkx.Graph()
-    draw_graw.add_nodes_from([1, 2, 3])
-    for i in g.graph_list:
-        draw_graw.add_edge(i[0], i[1])
-
-    networkx.draw_circular(draw_graw, node_color = 'red', node_size=1000,
-                           with_labels = True)
-
-
-
+    gl1 = [[0, 3],
+           [3, 0],
+           [3, 1],
+           [2, 3],
+           [3, 2],
+           [2, 1],
+           [1, 2],
+           [0, 0],
+           [1, 1]]
+    gl2 = [[0, 2], [2, 1], [1, 0]]
+    g1 = Graph(4, 9, gl1)
+    g2 = Graph(3, 3, gl2)
+    g1()
+    g2()
